3ce298c358adefa9d30bda9e49d7bf9cc4ffb595
NV4RE <tosbodesz@gmail.com>
2019-11-05 14:57:12 +0700

    Create index on start

--- src/store/elasticsearch/event.ts
+++ src/store/elasticsearch/event.ts
@@ -9,12 +9,186 @@ import { IAllEventWithId } from '../../kafka';
 //   R.pathOr([], ['hits', 'hits']),
 // );
 
+const ES_EVENTS_MAPPING = {
+  events: {
+    properties: {
+      details: {
+        properties: {
+          ackTimeout: {
+            type: 'date',
+          },
+          createTime: {
+            type: 'date',
+          },
+          endTime: {
+            type: 'date',
+          },
+          input: {
+            type: 'object',
+            enabled: false,
+          },
+          isRetried: {
+            type: 'boolean',
+          },
+          output: {
+            type: 'object',
+            enabled: false,
+          },
+          parallelTasks: {
+            type: 'object',
+            enabled: false,
+          },
+          retries: {
+            type: 'long',
+          },
+          retryDelay: {
+            type: 'long',
+          },
+          startTime: {
+            type: 'date',
+          },
+          status: {
+            type: 'keyword',
+          },
+          taskId: {
+            type: 'keyword',
+          },
+          taskName: {
+            type: 'keyword',
+          },
+          taskReferenceName: {
+            type: 'keyword',
+          },
+          timeout: {
+            type: 'long',
+          },
+          transactionId: {
+            type: 'keyword',
+          },
+          type: {
+            type: 'keyword',
+          },
+          workflowDefinition: {
+            properties: {
+              description: {
+                type: 'text',
+              },
+              failureStrategy: {
+                type: 'keyword',
+              },
+              name: {
+                type: 'keyword',
+              },
+              outputParameters: {
+                type: 'object',
+                enabled: false,
+              },
+              rev: {
+                type: 'keyword',
+              },
+              tasks: {
+                properties: {
+                  ackTimeout: {
+                    type: 'long',
+                  },
+                  inputParameters: {
+                    type: 'object',
+                    enabled: false,
+                  },
+                  name: {
+                    type: 'keyword',
+                  },
+                  parallelTasks: {
+                    properties: {
+                      ackTimeout: {
+                        type: 'long',
+                      },
+                      inputParameters: {
+                        type: 'object',
+                        enabled: false,
+                      },
+                      name: {
+                        type: 'keyword',
+                      },
+                      retry: {
+                        properties: {
+                          delay: {
+                            type: 'long',
+                          },
+                          limit: {
+                            type: 'long',
+                          },
+                        },
+                      },
+                      taskReferenceName: {
+                        type: 'keyword',
+                      },
+                      timeout: {
+                        type: 'long',
+                      },
+                      type: {
+                        type: 'keyword',
+                      },
+                    },
+                  },
+                  retry: {
+                    properties: {
+                      delay: {
+                        type: 'long',
+                      },
+                      limit: {
+                        type: 'long',
+                      },
+                    },
+                  },
+                  taskReferenceName: {
+                    type: 'keyword',
+                  },
+                  timeout: {
+                    type: 'long',
+                  },
+                  type: {
+                    type: 'keyword',
+                  },
+                },
+              },
+            },
+          },
+          workflowId: {
+            type: 'keyword',
+          },
+        },
+      },
+      isError: {
+        type: 'boolean',
+      },
+      timestamp: {
+        type: 'date',
+      },
+      transactionId: {
+        type: 'keyword',
+      },
+      type: {
+        type: 'keyword',
+      },
+    },
+  },
+};
+
 export class EventElasticsearchStore extends ElasticsearchStore
   implements IEventDataStore {
   index: string;
   constructor(config: any, index: string) {
     super(config);
     this.index = index;
+    this.client.indices
+      .create({
+        index: index,
+        body: {
+          mappings: ES_EVENTS_MAPPING,
+        },
+      })
+      .catch((error: any) => console.log(error.message));
   }
 
   get = async (transactionId: string): Promise<Event.AllEvent[]> => {
