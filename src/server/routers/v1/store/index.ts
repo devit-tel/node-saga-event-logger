938413f9e172df8df3dd47712ea41952bb03028d
NV4RE <tosbodesz@gmail.com>
2019-12-23 12:10:35 +0700

    Remove dup code

--- src/server/routers/v1/store/index.ts
+++ src/server/routers/v1/store/index.ts
@@ -27,8 +27,3 @@ router.get('/:transactionId', async (ctx: koaRouter.IRouterContext) => {
   const { transactionId } = ctx.params;
   return eventStore.getTransactionData(transactionId);
 });
-
-router.get('/:transactionId', async (ctx: koaRouter.IRouterContext) => {
-  const { transactionId } = ctx.params;
-  return eventStore.getTransactionData(transactionId);
-});
