dfd8034b325c3f050d092978e4a308d642e73216
NV4RE <tosbodesz@gmail.com>
2020-05-19 23:52:42 +0700

    one line

--- src/store/elasticsearch/event.ts
+++ src/store/elasticsearch/event.ts
@@ -408,7 +408,7 @@ export class EventElasticsearchStore extends ElasticsearchStore
       }, []),
     });
     if (resp.errors) {
-      console.log(resp.items);
+      console.log(JSON.stringify(resp.items));
       throw new Error('Fail to inserts');
     }
     return events;
